bc9a88c2c2a7584cb8dc572846f34afc391e9cdf
Sergey Poznyakoff <gray@gnu.org.ua>
2008-07-14 22:43:27 +0300

    Forgottent to add.
    * lib/xstream.c: New file.

---
+++ lib/xstream.c
@@ -0,0 +1,68 @@
+/* This file is part of Dico.
+   Copyright (C) 2008 Sergey Poznyakoff
+
+   Dico is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3, or (at your option)
+   any later version.
+
+   Dico is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with Dico.  If not, see <http://www.gnu.org/licenses/>. */
+
+#ifdef HAVE_CONFIG_H
+# include <config.h>
+#endif
+#include <xdico.h>
+#include <stdio.h>
+#include <ctype.h>
+
+int
+stream_writez(dico_stream_t str, char *buf)
+{
+    return dico_stream_write(str, buf, utf8_strbytelen(buf));
+}
+
+int
+stream_printf(dico_stream_t str, const char *fmt, ...)
+{
+    int len;
+    char *buf;
+    
+    va_list ap;
+    va_start(ap, fmt);
+    len = vasprintf(&buf, fmt, ap);
+    va_end(ap);
+    if (len < 0) {
+	dico_log(L_CRIT, 0,
+	       _("not enough memory while formatting reply message"));
+	exit(1);
+    }
+    len = dico_stream_write(str, buf, len);
+    free(buf);
+    return len;
+}
+
+void
+stream_write_multiline(dico_stream_t str, const char *text)
+{
+    struct utf8_iterator itr;
+    size_t len = 0;
+    
+    for (utf8_iter_first(&itr, (unsigned char *)text);
+	 !utf8_iter_end_p(&itr);
+	 utf8_iter_next(&itr)) {
+	if (utf8_iter_isascii(itr) && *itr.curptr == '\n') {
+	    dico_stream_writeln(str, itr.curptr - len, len);
+	    len = 0;
+	} else
+	    len += itr.curwidth;
+    }
+    if (len)
+	dico_stream_writeln(str, itr.curptr - len, len);
+}
+

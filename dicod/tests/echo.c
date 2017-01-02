f54aa87d0927a171d693e139966cfc45716622e8
Sergey Poznyakoff <gray@gnu.org>
2017-01-02 14:20:11 +0200

    Happy GNU Year

--- dicod/tests/echo.c
+++ dicod/tests/echo.c
@@ -1,5 +1,5 @@
 /* An "echo" database for GNU Dico test suite.
-   Copyright (C) 2012-2016 Sergey Poznyakoff
+   Copyright (C) 2012-2017 Sergey Poznyakoff
 
    GNU Dico is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
